e9018e837b5bfc0e7d16ca423d69492e3f44df51
toy <15968477+toyfer@users.noreply.github.com>
2023-10-05 12:49:54 +0900

    format

--- src/main.js
+++ src/main.js
@@ -1,45 +1,60 @@
-const { app, BrowserWindow, ipcMain, dialog, Menu, Notification, webContents } = require('electron');
-const { spawn } = require('child_process');
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
+const {
+    app,
+    BrowserWindow,
+    ipcMain,
+    dialog,
+    Menu,
+    Notification,
+    webContents,
+} = require("electron");
+const { spawn } = require("child_process");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
 // ランダム文字列を生成する関数（一時ファイル用）
 function generateRandomString(length) {
-    return [...Array(length).map(() => Math.random().toString(36)[2]).join('')];
-};
+    return [
+        ...Array(length)
+            .map(() => Math.random().toString(36)[2])
+            .join(""),
+    ];
+}
 
 // 描写・プリロード関数
 function createWindow() {
     // ブラウザウィンドウを作成
     mainWindow = new BrowserWindow({
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
+            preload: path.join(__dirname, "preload.js"),
+        },
     });
-    mainWindow.loadFile('./src/index.html');
+    mainWindow.loadFile("./src/index.html");
 
     // 開発者ツールを開く（オプション）
     // mainWindow.webContents.openDevTools();
-};
+}
 
 // アプリケーションの準備が完了したらウィンドウを作成
 app.whenReady().then(() => {
-    Menu.setApplicationMenu(null) // デフォルトのメニューを非表示
-    ipcMain.handle('dialog:openFile', handleFileOpen); // ファイル選択のリッスン
-    ipcMain.on('execute:runCommand', runCommand); // デバッグ用リッスン
-    ipcMain.on('execute:runFFmpeg', runFFmpeg); // FFmpeg用リッスン
-    ipcMain.on('execute:runWhisper', runWhisper); // Whisper用リッスン（使わない）
+    Menu.setApplicationMenu(null); // デフォルトのメニューを非表示
+    ipcMain.handle("dialog:openFile", handleFileOpen); // ファイル選択のリッスン
+    ipcMain.on("execute:runCommand", runCommand); // デバッグ用リッスン
+    ipcMain.on("execute:runFFmpeg", runFFmpeg); // FFmpeg用リッスン
+    ipcMain.on("execute:runWhisper", runWhisper); // Whisper用リッスン（使わない）
     createWindow(); // ウィンドウ作成
-    app.on('activate', function () {
+    app.on("activate", function () {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
-    mainWindow.webContents.send('process:Massage', `[${getNow()}:System]システムを起動しました`);
+    mainWindow.webContents.send(
+        "process:Massage",
+        `[${getNow()}:System]システムを起動しました`
+    );
 });
 
 // ウィンドウがすべて閉じられたらアプリを終了
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") app.quit();
 });
 
 // 実行セクション
@@ -61,62 +76,99 @@ const tempCSV = `${tempWAV}.csv`; // Whisperで出力されるCSVファイルの
 
 // FFmpegの実行
 function runFFmpeg(_event, args) {
-    const FFmpegArgs = `${path.join(__dirname, 'Whisper\\ffmpeg.exe')} -y -i ${args[0]} -ar 16000 ${tempWAV}`;
-    const process = spawn(`chcp 65001 && ${FFmpegArgs}`, [], { shell: true, windowsVerbatimArguments: true });
-    console.log(FFmpegArgs)
+    const FFmpegArgs = `${path.join(__dirname, "Whisper\\ffmpeg.exe")} -y -i ${args[0]
+        } -ar 16000 ${tempWAV}`;
+    const process = spawn(`chcp 65001 && ${FFmpegArgs}`, [], {
+        shell: true,
+        windowsVerbatimArguments: true,
+    });
+    console.log(FFmpegArgs);
 
     // 標準出力リッスン
-    process.stdout.on('data', (data) => {
+    process.stdout.on("data", (data) => {
         console.log(`[${getNow()}:FFmpeg]${data}`);
-        mainWindow.webContents.send('return:Command', `[${getNow()}:FFmpeg]${data}`);
+        mainWindow.webContents.send(
+            "return:Command",
+            `[${getNow()}:FFmpeg]${data}`
+        );
     });
 
     // エラー出力リッスン
-    process.stderr.on('data', (data) => {
+    process.stderr.on("data", (data) => {
         console.log(`[${getNow()}:FFmpeg]${data}`);
-        mainWindow.webContents.send('return:Command', `[${getNow()}:FFmpeg]${data}`);
+        mainWindow.webContents.send(
+            "return:Command",
+            `[${getNow()}:FFmpeg]${data}`
+        );
     });
 
     // 終了時リッスン
-    process.on('close', (code) => {
+    process.on("close", (code) => {
         // エラーハンドリング
         if (code != 0) {
-            mainWindow.webContents.send('process:Massage', `[${getNow()}:FFmpeg]エラーが発生しました\n errorcode:${code}`);
+            mainWindow.webContents.send(
+                "process:Massage",
+                `[${getNow()}:FFmpeg]エラーが発生しました\n errorcode:${code}`
+            );
             return;
         }
         console.log(`[${getNow()}:FFmpeg]child process exited with code ${code}`);
-        mainWindow.webContents.send('return:Command', `[${getNow()}:FFmpeg]音声処理が完了しました`);
-        runWhisper(args) // FFmpegの実行が完了時、Whisperを実行する
+        mainWindow.webContents.send(
+            "return:Command",
+            `[${getNow()}:FFmpeg]音声処理が完了しました`
+        );
+        runWhisper(args); // FFmpegの実行が完了時、Whisperを実行する
     });
 }
 
 // Whisperの実行
 function runWhisper(args) {
-    const WhisperArgs = `${path.join(__dirname, 'Whisper\\python.exe')} ${path.join(__dirname, args[1].script)} ${path.join(__dirname, args[1].model)} ${tempWAV}`;
-    const process = spawn(`chcp 65001 && ${WhisperArgs}`, [], { shell: true, windowsVerbatimArguments: true });
+    const WhisperArgs = `${path.join(
+        __dirname,
+        "Whisper\\python.exe"
+    )} ${path.join(__dirname, args[1].script)} ${path.join(
+        __dirname,
+        args[1].model
+    )} ${tempWAV}`;
+    const process = spawn(`chcp 65001 && ${WhisperArgs}`, [], {
+        shell: true,
+        windowsVerbatimArguments: true,
+    });
 
     // 標準出力
-    process.stdout.on('data', (data) => {
+    process.stdout.on("data", (data) => {
         console.log(`[${getNow()}:Whisper]${data}`);
-        mainWindow.webContents.send('return:Command', `[${getNow()}:Whisper]${data}`);
+        mainWindow.webContents.send(
+            "return:Command",
+            `[${getNow()}:Whisper]${data}`
+        );
     });
 
     // エラー出力
-    process.stderr.on('data', (data) => {
+    process.stderr.on("data", (data) => {
         console.log(`[${getNow()}:Whisper]${data}`);
-        mainWindow.webContents.send('return:Command', `[${getNow()}:Whisper]${data}`);
+        mainWindow.webContents.send(
+            "return:Command",
+            `[${getNow()}:Whisper]${data}`
+        );
     });
 
     // 終了時出力
-    process.on('close', (code) => {
+    process.on("close", (code) => {
         // エラーハンドリング
         if (code != 0) {
-            mainWindow.webContents.send('process:Massage', `[${getNow()}:Whisper]エラーが発生しました\n errorcode:${code}`);
+            mainWindow.webContents.send(
+                "process:Massage",
+                `[${getNow()}:Whisper]エラーが発生しました\n errorcode:${code}`
+            );
             fs.unlinckSync(tempWAV); // 一時ファイルを削除
             return;
         }
         console.log(`[${getNow()}:Whisper]child process exited with code ${code}`);
-        mainWindow.webContents.send('return:Command', `[${getNow()}:Whisper]文字起こしが完了しました`);
+        mainWindow.webContents.send(
+            "return:Command",
+            `[${getNow()}:Whisper]文字起こしが完了しました`
+        );
         if (fs.existsSync(tempWAV)) {
             fs.unlinkSync(tempWAV);
         }
@@ -127,22 +179,28 @@ function runWhisper(args) {
 // 最終調整実行
 function runAdjustment(args) {
     // tmpファイルのパスを取得
-    const outFile = `${args[0]}_[${getNow(true)}].csv`
+    const outFile = `${args[0]}_[${getNow(true)}].csv`;
     fs.copyFile(tempCSV, outFile, (err) => {
         if (err) {
-            mainWindow.webContents.send('process:Massage', `[${getNow()}:System]${err}`)
+            mainWindow.webContents.send(
+                "process:Massage",
+                `[${getNow()}:System]${err}`
+            );
             if (fs.existsSync(tempWAV)) {
                 fs.unlinkSync(tempWAV);
             }
         } else {
-            mainWindow.webContents.send('process:Massage', `[${getNow()}:System]文字起こしが完了しました`);
+            mainWindow.webContents.send(
+                "process:Massage",
+                `[${getNow()}:System]文字起こしが完了しました`
+            );
             if (fs.existsSync(tempWAV)) {
                 fs.unlinkSync(tempWAV);
             }
-            return
+            return;
         }
     });
-};
+}
 
 // 時刻の取得関数
 function getNow(pathFlag = null) {
@@ -156,9 +214,8 @@ function getNow(pathFlag = null) {
     const sec = now.getSeconds();
 
     if (!pathFlag) {
-        return `${year}/${month}/${date}_${hour}:${min}:${sec}`
+        return `${year}/${month}/${date}_${hour}:${min}:${sec}`;
     } else {
-        return `${year}-${month}-${date}_${hour}-${min}-${sec}`
+        return `${year}-${month}-${date}_${hour}-${min}-${sec}`;
     }
 }
-
